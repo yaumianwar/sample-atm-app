5a2074551a557a7fcf70789071bf3b2bf448fdb9
Nur Yaumi Anwar <nuryaumi10@gmail.com>
2016-01-20 16:53:44 +0800

    push to github

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ database.init_app(app)
 
 @app.route("/")
 def homepage():
-  user = User.query.filter_by(id=1).first()
-  return render_template("home.html",  **locals())
+  	user = User.query.filter_by(id=1).first()
+  	return render_template("home.html",  **locals())
 
 @app.route("/regist", methods=["POST", "GET"])
 def regist():
@@ -26,61 +26,61 @@ def regist():
 		avatar = request.form.get("avatar", None)
 
 		errors = []
-    if full_name is None and full_name == "":
-      errors.append(dict(field="full_name",
-                        message="Input Empty"))
-    if email is None and email == "":
-      errors.append(dict(field="email",
-                        message="Input Empty"))
-    if password is None and password == "":
-      errors.append(dict(field="passwor",
-                        message="Input Empty")) 
-    if password != confirm_password:
-      errors.append(dict(field="confirm_password",
-                        message="Password and Confirm Password Should be the same"))
-    if bio is None and bio == "":
-      errors.append(dict(field="bio",
-                        message="Input Empty"))
-    if avatar is None avatar == "":
-      errors.append(dict(field="avatar",
-                        message="Input Empty"))   
-    if len(errors) > 0:
-      return render_template("register.html", **locals())
+    	if full_name is None and full_name == "":
+      		errors.append(dict(field="full_name",
+                        		message="Input Empty"))
+    	if email is None and email == "":
+      		errors.append(dict(field="email",
+                        		message="Input Empty"))
+    	if password is None and password == "":
+      		errors.append(dict(field="passwor",
+                        		message="Input Empty")) 
+    	if password != confirm_password:
+      		errors.append(dict(field="confirm_password",
+                        		message="Password and Confirm Password Should be the same"))
+    	if bio is None and bio == "":
+      		errors.append(dict(field="bio",
+                        		message="Input Empty"))
+    	if avatar is None and avatar == "":
+   		errors.append(dict(field="avatar",
+                        		message="Input Empty"))   
+    	if len(errors) > 0:
+      		return render_template("register.html", **locals())
 
-    user = User(full_name, email, password, bio, avatar)
-    db.session.add(user)
-    db.session.commit()
+    	user = User(full_name, email, password, bio, avatar)
+    	db.session.add(user)
+    	db.session.commit()
 
-    session["user_id"] = user.id
-    return redirect("home.html", **locals())
+    	session["user_id"] = user.id
+    	return redirect("home.html", **locals())
 
   # return render_template("register.html", **locals())
 
 @app.route("/login", methods = "GET")
 def login():
-  email = request.form.get("email", None)
-  password = request.form.get("password", None)
-  if email != "" or password != "":
-    user = User.query.filter_by(email=email).first()
-    if user is not None:
-      #verify user password
-      if user.password == password:
-        # update user.last_login to now
-        user.last_login = datetime.datetime.now()
-        db.session.add(user)
-        db.session.commit()
-        # save user id
-        session["user_id"] = user.id
-        return redirect("/")
+  	email = request.form.get("email", None)
+  	password = request.form.get("password", None)
+  	if email != "" or password != "":
+    		user = User.query.filter_by(email=email).first()
+    		if user is not None:
+      		#verify user password
+      			if user.password == password:
+        			# update user.last_login to now
+        			user.last_login = datetime.datetime.now()
+        			db.session.add(user)
+        			db.session.commit()
+        			# save user id
+        			session["user_id"] = user.id
+        			return redirect("/")
 
-      else:
-        return render_template("login.html", **locals())  
+      			else:
+        			return render_template("login.html", **locals())  
 
-    else:     
-      return render_template("login.html", **locals())
+    		else:     
+      			return render_template("login.html", **locals())
 
-  else:
-    return render_template("login.html", **locals())  
+  	else:
+    		return render_template("login.html", **locals())  
 
 
 if __name__=="__main__":
